b7c646e66465449a2d597b81c95e9ef79d3a9edd
Pratik Panda <38151379+MrPanda1@users.noreply.github.com>
2021-01-13 17:36:32 -0700

    Cleaned up init

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 import discord
-import os
 from dotenv import load_dotenv
+import json
+import os
 
 # Load vars from .env file into environment variables
 load_dotenv()
+
+# Get config vars
+config = json.load(open('config.json', 'r'))
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    print(config)
 
 @client.event
 async def on_message(message):
-    # If I wrote the message, do nothing
+    # Make sure we have the proper prefix
+    if not message.content.startswith(config['prefix']):
+        return
+
+    # If the bot wrote the message, do nothing
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    # Get command and args for cmd here
+    try:
+        cmd, args = message.content[len(config['prefix']) : len(message.content)].split(None, 1)
+    except ValueError:
+        cmd = message.content[len(config['prefix']) : len(message.content)]
+        args = None
+
+    if cmd.lower() == 'ping':
+        await message.channel.send('Pong')
+    elif cmd.lower() == 'echo':
+        await message.channel.send(f'Echoing: {args}')
     
 client.run(os.getenv('TOKEN'))
